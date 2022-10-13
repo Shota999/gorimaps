f66f2c6258849c6a045ad28bf066d8be62da1f84
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-10-13 12:23:11 +0400

    main

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,20 @@
+import { GoogleMap, useLoadScript, Marker} from "@react-google-maps/api";
+
 const Map = () => {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyAcTB6AOoQSMG5YPHVKc67CpvTEQQJyWBM"
+    });
+    if( !isLoaded ) return <div>Loading ...</div>
+
     return ( 
-        <div>
-            
-        </div>
+            <GoogleMap 
+                zoom={15} 
+                center={{lat: 41.725307786180615, lng: 44.7626359520697}} 
+                mapContainerClassName="map_container"
+            >
+                <Marker position={{lat: 41.725307786180615, lng: 44.7626359520697}} />
+            </GoogleMap>
      );
 }
- 
+// 41.725307786180615, 44.7626359520697
 export default Map;
